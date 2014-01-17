846486291898fca989e5597f324b4ffdd03c7990
transontt@gmail.com <transontt@gmail.com@187ae917-9fd3-e397-0006-fc07bd25ac82>
2014-01-17 13:04:08 +0000

    Them link tinh vao menu ben trai cho muc tin tuc

--- trunk/catalog/controller/common/header.php
+++ trunk/catalog/controller/common/header.php
@@ -59,6 +59,7 @@ class ControllerCommonHeader extends Controller {
         $this->data['checkout'] = $this->url->link('checkout/checkout', '', 'SSL');
         $this->data['news_brand_href'] = $this->url->link('news/news_category', 'cat_id=2', 'SSL');
         $this->data['lookbook_href'] = $this->url->link('news/news_category', 'cat_id=3', 'SSL');
+        $this->data['news_href'] = $this->url->link('news/news_category', 'cat_id=4', 'SSL');
         // Daniel's robot detector
         $status = true;
 
